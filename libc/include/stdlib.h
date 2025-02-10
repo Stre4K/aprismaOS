9255d6b921ee8dc0166b3081287dae32d031ee08
Stre4K <eric.strihagen@gmail.com>
2025-02-10 01:11:42 +0100

    Add errno.h following the posix standard

--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -2,13 +2,13 @@
 #define _STDLIB_H 1
 
 #include <sys/cdefs.h>
+#include <sys/errno.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-__attribute__((__noreturn__))
-void abort(void);
+__attribute__((__noreturn__)) void abort(void);
 
 #ifdef __cplusplus
 }
