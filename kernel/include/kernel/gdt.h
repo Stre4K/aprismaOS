c961460cba370deeed97cc87bda1f039eb6625e2
Stre4K <107279930+Stre4K@users.noreply.github.com>
2025-02-20 20:54:19 +0100

    Change setGdtGate to snake case
    The new function can be called with set_gdt_gate().

--- kernel/include/kernel/gdt.h
+++ kernel/include/kernel/gdt.h
@@ -67,7 +67,7 @@ struct gdt_ptr_struct {
 
 
 void init_gdt();
-void setGdtGate(uint32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t flags);
+void set_gdt_gate(uint32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t flags);
 void create_descriptor(uint32_t base, uint32_t limit, uint16_t flags);
 
 #endif
