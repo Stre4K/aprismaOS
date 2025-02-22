b84443bb4d1088dce216283c3d9ba3ef963b87af
Stre4K <eric.strihagen@gmail.com>
2025-02-22 12:58:39 +0100

    Add qsort to stdlib
    This commit add first version of quicksort to the stdlib.

--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -2,6 +2,7 @@
 #define _STDLIB_H 1
 
 #include <errno.h>
+#include <stddef.h>
 #include <sys/cdefs.h>
 
 #ifdef __cplusplus
@@ -12,6 +13,8 @@ extern "C" {
 #define EXIT_FAILURE 1
 
 __attribute__((__noreturn__)) void abort(void);
+void qsort(void *ptr, size_t count, size_t size,
+           int (*comp)(const void *, const void *));
 
 #ifdef __cplusplus
 }
