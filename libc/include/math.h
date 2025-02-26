4e7676c4e3af2d8aea89d2adfaff84787b529b49
Stre4K <107279930+Stre4K@users.noreply.github.com>
2025-02-26 10:46:45 +0100

    Add abs function to libc
    This commit add abs (absolute value) function to libc math.

--- libc/include/math.h
+++ libc/include/math.h
@@ -1,14 +1,12 @@
 // math.h
 // Author: Stre4K
-// Date: 2025-02-23
+// Date: 2025-02-26
 
 #ifndef _MATH_H
 #define _MATH_H
 
 int pow(int base, int exponent);
 double sqrt(double x);
+int abs(int x);
 
-
-
-
-#endif _MATH_H
+#endif  //_MATH_H
