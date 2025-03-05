13eb61f50086d552d5e7628fd958d8f0ad608586
Stre4K <eric.strihagen@gmail.com>
2025-03-05 08:21:42 +0100

    Add fabs fucntion to libc
    This commit add fabs function to libc math. The fabs function returns a
    floating point absolute value.

--- libc/include/math.h
+++ libc/include/math.h
@@ -1,6 +1,6 @@
 // math.h
 // Author: Stre4K
-// Date: 2025-03-04
+// Date: 2025-03-05
 
 #ifndef _MATH_H
 #define _MATH_H
@@ -8,6 +8,7 @@
 int pow(int base, int exponent);
 double sqrt(double x);
 int abs(int x);
+double fabs(double x);
 int max(int a, int b);
 int min(int a, int b);
 double floor(double x);
