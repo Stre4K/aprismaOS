f2a0061f812e12be00dfb06f3bda1aaf8ad5c7c4
Stre4K <eric.strihagen@gmail.com>
2025-03-07 10:11:03 +0100

    Add log function to libc
    This commit add logarithmic function to libc math.

---
+++ libc/math/log.c
@@ -0,0 +1,23 @@
+// log.c
+// <math.h>
+// Author: Stre4K
+// Date: 2025-03-06
+
+#include <math.h>
+
+// Natural logarithm approximation using Newton's method
+double log(double x) {
+    if (x <= 0) return 0;  // log is undefined for x <= 0
+
+    double result = x - 1.0;  // Initial guess
+    double epsilon = 1e-10;
+
+    for (int i = 0; i < 100; i++) {  // Limit iterations
+        double e_pow = exp(result);  // We'll implement exp() next
+        double diff = (e_pow - x) / e_pow;
+        if (diff > -epsilon && diff < epsilon) break;
+        result -= diff;
+    }
+
+    return result;
+}
