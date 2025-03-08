b5b7666b9b4946a1632870cf61719eb6d1b75a5d
Stre4K <eric.strihagen@gmail.com>
2025-03-08 15:27:08 +0100

    Update pow function input from int to double

--- libc/math/pow.c
+++ libc/math/pow.c
@@ -5,10 +5,29 @@
 
 #include <math.h>
 
-int pow(int base, int exponent) {
-    int result = 1;
-    for (int i = 0; i < exponent; i++) {
-        result *= base;
+// Helper function for integer exponents
+double pow_int(double base, int exponent){
+    double result = 1.0;
+    int exp = (exponent < 0) ? -exponent : exponent;
+
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            result *= base;
+        }
+        base *= base;
+        exp /= 2;
     }
-    return result;
+
+    return (exponent < 0) ? 1.0 / result : result;
+}
+
+
+// Main pow function
+double pow(double base, double exponent) {
+    if (exponent == (int)exponent) {
+        return pow_int(base, (int)exponent);
+    }
+
+    // For non-integer exponents
+    return exp(base * log(exponent));
 }
