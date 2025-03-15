3f148917e64c5513ac9e7565f5a19372dbc3e3d8
Stre4K <107279930+Stre4K@users.noreply.github.com>
2025-03-15 14:04:27 +0100

    Add ctype header and isdigit function to libc
    This commit add the ctype.h and isdigit function to libc.

---
+++ libc/ctype/isdigit.c
@@ -0,0 +1,10 @@
+// isdigit.c
+// <ctype.h>
+// Author: Stre4K
+// Date: 2025-03-15
+
+#include <ctype.h>
+
+int isdigit(int c) {
+    return c >= '0' && c <= '9';
+}
---
+++ libc/include/ctype.h
@@ -0,0 +1,25 @@
+// ctype.h
+// Author: Stre4K
+// Date: 2025-03-15
+
+#ifndef __CTYPE_H
+#define __CTYPE_H
+
+int isalnum(int c);
+int isalpha(int c);
+int isascii(int c);
+int isblank(int c);
+int iscntrl(int c);
+int isdigit(int c);
+int isgraph(int c);
+int islower(int c);
+int isprint(int c);
+int ispunct(int c);
+int isspace(int c);
+int isupper(int c);
+int isxdigit(int c);
+int toascii(int c);
+int tolower(int c);
+int toupper(int c);
+
+#endif  //__CTYPE_H
