557e6a4c237d62ec246130a204a0bd8aaa1eeb79
Stre4K <107279930+Stre4K@users.noreply.github.com>
2025-09-24 18:50:00 +0200

    fix(libc): correct cbrt() implementation to return a value
    - Ensures proper behavior and resolves -Werror build failure

--- libc/math/sqrt.c
+++ libc/math/sqrt.c
@@ -7,11 +7,11 @@
 
 double sqrt(double x) {
     if (x < 0) {
-        return -1; // ERROR sqrt of negative number
+        return -1;  // ERROR sqrt of negative number
     }
 
     double guess = x;
-    double epsilon = 0.00001; // Precision
+    double epsilon = 0.00001;  // Precision
 
     while ((guess * guess - x > epsilon) || (x - guess * guess > epsilon)) {
         guess = (guess + x / guess) / 2;
@@ -22,13 +22,16 @@ double sqrt(double x) {
 
 double cbrt(double x) {
     if (x < 0) {
-        return -1; // ERROR cbrt of negative number
+        return -1;  // ERROR cbrt of negative number
     }
 
     double guess = x;
-    double epsilon = 0.00001; // Precision
+    double epsilon = 0.00001;  // Precision
 
-    while ((guess * guess * guess - x > epsilon) || (x - guess * guess * guess > epsilon)) {
+    while ((guess * guess * guess - x > epsilon) ||
+           (x - guess * guess * guess > epsilon)) {
         guess = (guess + x / (guess * guess)) / 2;
     }
+
+    return guess;
 }
